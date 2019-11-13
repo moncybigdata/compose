f5a4f2233c482f51c20c5cc6c23a8bbce0714c36
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-11-13 13:52:41 -0600

    bump demo version

--- composeml/__init__.py
+++ composeml/__init__.py
@@ -3,4 +3,4 @@ from . import demos
 from .label_maker import LabelMaker
 from .label_times import LabelTimes, read_csv, read_parquet, read_pickle
 
-__version__ = '0.0.0.dev2'
+__version__ = '0.0.0.dev3'
